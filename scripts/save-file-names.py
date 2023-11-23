82c80c7eb89aaa392b71c7f1140e66f992c36bbe
simply-pleb <52733476+simply-pleb@users.noreply.github.com>
2023-11-23 06:11:56 +0300

    add file name saver

---
+++ scripts/save-file-names.py
@@ -0,0 +1,22 @@
+import os
+
+def list_files(directory):
+    file_names = []
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            file_names.append(filename)
+    return file_names
+
+# Replace 'your_directory_path' with the path to the directory you want to explore
+directory_path = 'raw-data/'
+
+file_names = list_files(directory_path)
+
+file_names_output_path = 'raw-data/raw-data-file-name.txt'
+
+
+with open(file_names_output_path, 'w') as file:
+    for name in sorted(file_names):
+        file.write(name + '\n')
+
+print(f"File names saved to {file_names_output_path}")
